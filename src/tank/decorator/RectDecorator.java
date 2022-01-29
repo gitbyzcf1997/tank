769856930f05e50256d4e24c3c431ec613c72110
zcf <2272026094@qq.com>
2022-01-29 23:44:52 +0800

    加入装饰器模式 动态的添加子弹的装饰

--- src/tank/decorator/RectDecorator.java
+++ src/tank/decorator/RectDecorator.java
@@ -17,6 +17,7 @@ public class RectDecorator extends GODecorator {
     }
     @Override
     public void paint(Graphics g){
+        //因为子弹的x，y是动态的 需要时刻更新x，y
         this.x=go.x;
         this.y=go.y;
         //因为父类聚合了GameObject
@@ -25,7 +26,7 @@ public class RectDecorator extends GODecorator {
         //画想要的装饰
         Color c = g.getColor();
         g.setColor(Color.YELLOW);
-        g.drawRect(super.go.x,super.go.y,super.getWidth()+2,super.getHeight()+2);
+        g.drawRect(go.x,go.y,getWidth()+2,getHeight()+2);
         g.setColor(c);
     }
 }
--- src/tank/strategy/DefaultFireStrategy.java
+++ src/tank/strategy/DefaultFireStrategy.java
@@ -20,9 +20,10 @@ public class DefaultFireStrategy implements FireStrategy {
     public void fire(Tank t) {
         int bX=t.getX()+Tank.WIDTH/2-Bullet.WIDTH/2;
         int bY=t.getY()+Tank.HEIGHT/2-Bullet.HEIGHT/2;
-        GameModel.getINSTANCE().add(new RectDecorator(
-                new TailDecorator(
-                new Bullet(bX,bY,t.getDir(),t.getGroup()))));
+//        GameModel.getINSTANCE().add(new RectDecorator(
+//                new TailDecorator(
+//                new Bullet(bX,bY,t.getDir(),t.getGroup()))));
+        new Bullet(bX,bY,t.getDir(),t.getGroup());
         //if(t.getGroup()==Group.GOOD)new Thread(()->new Audio("audio/tank_fire.wav").play()).start();
     }
 
