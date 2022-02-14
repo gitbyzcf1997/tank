92e43d986fb666026c64c0eb23c6720172761e71
zcf <2272026094@qq.com>
2022-02-14 18:16:34 +0800

    将敌人坦克取消，为网络版做准备

--- src/tank/Main.java
+++ src/tank/Main.java
@@ -17,10 +17,10 @@ public class Main {
         TankFrame tankFrame = new TankFrame();
         PropertMgr pg = PropertMgr.getPropertMgr();
         int initTankCount= pg.getInt("initTankCount");
-        //初始化敌方坦克
-        for(int i=0;i<initTankCount;i++){
-            tankFrame.tanks.add(new Tank((i*ResourceMgr.badtankU.getWidth())*2,150,Dir.DOWN,tankFrame,Group.BAD));
-        }
+//        //初始化敌方坦克
+//        for(int i=0;i<initTankCount;i++){
+//            tankFrame.tanks.add(new Tank((i*ResourceMgr.badtankU.getWidth())*2,150,Dir.DOWN,tankFrame,Group.BAD));
+//        }
         new Thread(()->{new Audio("audio/war1.wav").loop();}).start();
         while (true){
             Thread.sleep(50);
